0fc5c6b04abcbf43acb9f8653ef541d818742850
PaarthBhasin <90496636+PaarthBhasin@users.noreply.github.com>
2023-08-30 15:09:53 +0530

    build:completed infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -26,7 +26,7 @@ export class News extends Component {
       articles:[],
       loading:false,
       page:1,
-      totalResults:0
+      // totalResults:0
     }
     document.title= `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
@@ -92,6 +92,19 @@ export class News extends Component {
     this.updateNews();
   }
 
+  fetchMoreData = async() => {
+   this.setState({page:this.state.page+1})
+   const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9083c3ed45cc4d2592eb9e51a4ef77dd&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data = await fetch(url);
+    let parsedData = await data.json();
+      this.setState({
+        articles: this.state.articles.concat(parsedData.articles),
+        loading:false,
+        totalResults: parsedData.totalResults
+      })
+  };
+
   render() {
     return (
       <div className='container my-3'>
@@ -104,12 +117,14 @@ export class News extends Component {
           hasMore={this.state.articles.length !== this.state.totalResults}
           loader={<Spinner/>}
         >
+          <div className="container">
         <div className="row">
           {this.state.articles.map((element)=>{
            return <div className="col-md-4" key={element.url}>
             <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
           </div>
           })}
+        </div>
         </div>
         </InfiniteScroll>
         <div className="container d-flex justify-content-between">
